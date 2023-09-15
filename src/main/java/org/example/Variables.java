a2fe3e43c73286cd3ea63989c103bdb7ee238e1a
jirizach <zach.2@seznam.cz>
2023-09-15 14:14:28 +0200

    Variables

---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        byte  myFirstByte  = 2 ;
+        short myFirstShort = 2 ;
+        int   myFirstInt   = 2 ;
+        long  myFirstLong  = 2 ;
+
+        Byte objByte = Byte.MAX_VALUE;
+        Short objShort = Short.MAX_VALUE;
+        Integer objInteger = Integer.MAX_VALUE;
+        Long objLong = Long.MAX_VALUE;
+
+        System.out.println("Byte max " + objByte);
+        System.out.println("Short max " + objShort);
+        System.out.println("Int max " + objInteger);
+        System.out.println("Long max " + objLong);
+    }
+}
